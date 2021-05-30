005ebe74fdc73b2c40a61f2e32b6a52326d0b7fd
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-30 20:29:03 +0200

    help message and help command

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,26 @@ use serenity::{
     prelude::*,
 };
 
+const HELP_MESSAGE: &str = "
+Hello there, Human!
+
+You have summoned me. Let's see about getting you what you need.
+
+? Need technical help?
+==> Post is the <#CHANNEL_ID> channel and other humans will assit you.
+
+? Looking for the Code of Conduct?
+==> Here it is: <Place_Holder>
+
+? Something wrong?
+==> You can flag an admin with @admin
+
+I hope that resolves your issue!
+-- Helpbot
+";
+
+const HELP_COMMAND: &str = "!help";
+
 fn main() {
     println!("Hello, world!");
 }
