c169f06f5f5500c27f40e60045512c508dc0e1b2
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-30 20:35:17 +0200

    Eventhandler

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,23 @@ I hope that resolves your issue!
 
 const HELP_COMMAND: &str = "!help";
 
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == HELP_COMMAND {
+            if let Err(why) = msg.channel_id.say(&ctx.http, HELP_MESSAGE).await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
